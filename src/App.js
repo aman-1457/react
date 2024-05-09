04afc4bd86d57c16bd406a8e00792c921ea8a51c
aman-1457 <aman@rasheedfoundation.org>
2024-05-09 21:38:36 +0530

    task2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div> 
+    <h1>Welcome to my react app!</h1>
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+// Hello.js
+import React from 'react';
+
+function Hello() {
+  return(
+    <div>
+        <p>Hello World!</p>
+    </div>
+  );
+
+}
+
+
+
+
+
+export default Hello;
